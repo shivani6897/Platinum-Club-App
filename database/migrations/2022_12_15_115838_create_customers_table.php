69c736edb813141e98f261b66310dbc2b2d72c83
krisha-72 <98090608+krisha-72@users.noreply.github.com>
2022-12-15 17:35:20 +0530

    Migration: Create customers table

---
+++ database/migrations/2022_12_15_115838_create_customers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('company_name');
+            $table->string('email');
+            $table->string('phone_no');
+            $table->string('gst_no')->nullable();
+            $table->string('state');
+            $table->string('company_address')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
