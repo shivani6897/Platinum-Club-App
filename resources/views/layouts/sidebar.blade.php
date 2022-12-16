54cb6d26bce52a24535e8c7d328042cc7663cbc3
krisha-72 <98090608+krisha-72@users.noreply.github.com>
2022-12-16 19:45:31 +0530

    fix: add incomes in my business in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -59,7 +59,7 @@
             class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
             x-tooltip.placement.right="'Habits'"
         >
-<i class="fa-solid fa-clock fa-2x"></i>
+            <i class="fa-solid fa-clock fa-2x"></i>
         </a>
 
       <!-- Events -->
@@ -73,7 +73,7 @@
 
         <!-- My Business -->
         <a
-          href="{{url('business')}}"
+          href="{{url('incomes')}}"
           class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:hover:bg-navy-300/20 dark:focus:bg-navy-300/20 dark:active:bg-navy-300/25"
           x-tooltip.placement.right="'My Business'"
         >
