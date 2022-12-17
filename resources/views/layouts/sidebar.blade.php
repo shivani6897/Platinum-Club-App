48b32ad1ed0a8fc851cfc4e640cc32456d06b069
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2022-12-17 09:19:28 +0530

    fix: customer add from user panel field error fixed

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
       <a href="/">
         <img
           class="h-11 w-11 transition-transform duration-500 ease-in-out hover:rotate-[360deg]"
-          src="{{asset('images/app-logo.svg')}}"
+          src="{{asset('images/app-logo.png')}}"
           alt="logo"
         />
       </a>
