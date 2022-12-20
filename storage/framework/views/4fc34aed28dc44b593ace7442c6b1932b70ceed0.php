71ae96a977dc360824ab47b970a01e0b728ae88f
sandywebdev <80840989+sandywebdev@users.noreply.github.com>
2022-12-20 12:26:12 +0530

    add subscription  page designed

--- storage/framework/views/4fc34aed28dc44b593ace7442c6b1932b70ceed0.php
+++ storage/framework/views/4fc34aed28dc44b593ace7442c6b1932b70ceed0.php
@@ -1,3 +1,4 @@
+
 <?php $__env->startSection('heading', 'Products'); ?>
 
 <?php $__env->startSection('content'); ?>
--- storage/framework/views/9d3eec70fd952ccad0c6b3790ea05da145d34554.php
+++ storage/framework/views/9d3eec70fd952ccad0c6b3790ea05da145d34554.php
@@ -45,7 +45,7 @@
         <!-- TODO Subscription -->
         <a href="<?php echo e(route('subscription.index')); ?>"
         class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
-        x-tooltip.placement.right="'Products'">
+        x-tooltip.placement.right="'Subscription'">
             <i class="fa-sharp fa-solid fa-s fa-2x"></i>
       </a>
 
