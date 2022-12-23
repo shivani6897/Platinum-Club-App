c27a11ec12824a58a834cda26f7636271cd7d36b
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2022-12-23 13:48:12 +0530

    test: validation change in admin panel user module

--- app/Http/Requests/Admin/User/StoreRequest.php
+++ app/Http/Requests/Admin/User/StoreRequest.php
@@ -24,7 +24,7 @@ class StoreRequest extends FormRequest
     public function rules()
     {
         return [
-             'club_id'=>'required|exists:clubs,id',
+             // 'club_id'=>'required|exists:clubs,id',
              'first_name'=>'required', 'string', 'max:255',
              'phone_no'=>'required',
              'last_name' => 'required', 'string', 'max:255',
