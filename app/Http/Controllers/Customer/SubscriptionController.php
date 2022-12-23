f06998221d663e819707746fc49c65705fd53903
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2022-12-23 11:46:13 +0530

    chore:remove extra files

---
+++ app/Http/Controllers/Customer/SubscriptionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Models\RecurringInvoice;
+
+class SubscriptionController extends Controller
+{
+    public function index()
+    {
+        // $rinvoices = RecurringInvoice::where('user_id',auth()->id())->paginate(10);
+        // return view('subscriptions',compact('$rinvoices'));
+    }
+}
