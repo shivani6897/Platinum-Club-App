b08586ddeb8ff86ba2dcb0022befacb963365d9a
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2022-12-23 13:36:41 +0530

    fix: admin profile issue

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -47,7 +47,7 @@
 		                  		d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z">
 		                  	</path>
                 		</svg>
-                    	<span>{{Auth::user()->created_at->format('jS F, Y')}}</span>
+                    	<span>{{Auth::user()->created_at?->format('jS F, Y')}}</span>
                   	</a>
                 </li>
                 <li>
