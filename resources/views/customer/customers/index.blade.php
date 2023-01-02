35862d57efc2cf3fd24649df4bd083870b9e10c6
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2023-01-02 23:59:32 +0530

    fix: user panel customers index page column name changed to created at

--- resources/views/customer/customers/index.blade.php
+++ resources/views/customer/customers/index.blade.php
@@ -106,7 +106,7 @@
                                 class="table-sort whitespace-nowrap bg-slate-200 px-4 py-3 font-semibold uppercase text-slate-800 dark:bg-navy-800 dark:text-navy-100 lg:px-5"
                             >
 {{--                            <th class="whitespace-nowrap bg-slate-200 px-4 py-3 font-semibold uppercase text-slate-800 dark:bg-navy-800 dark:text-navy-100 lg:px-5">--}}
-                                Date
+                                Created At
                             </th>
                             <th class=" table-sortable whitespace-nowrap bg-slate-200 px-4 py-3 font-semibold uppercase text-slate-800 dark:bg-navy-800 dark:text-navy-100 lg:px-5">
                                 Name
