e42fd6635f6fd2dc599f3df40a9e82fceff2ae3e
Kanaiya Rana <kanaiya.codepaper@gmail.com>
2023-01-07 13:58:37 +0530

    chore:remove promocode menu from sidebar as its under development

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -140,13 +140,13 @@
         </a> --}}
 
         {{--    promocode gateway--}}
-        <a
+        {{-- <a
             href="{{route('promocodes.index')}}"
             class="flex h-11 w-11 items-center justify-center rounded-lg outline-none transition-colors duration-200 hover:bg-primary/20 focus:bg-primary/20 active:bg-primary/25 dark:bg-navy-600 dark:hover:bg-navy-450 dark:focus:bg-navy-450 dark:active:bg-navy-450/90"
             x-tooltip.placement.right="'Promocode'"
         >
             <i class="fa-solid fa-tag fa-2x"></i>
-        </a>
+        </a> --}}
 
 
 {{--    payment gateway--}}
